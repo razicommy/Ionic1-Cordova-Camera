47373649b7d21099d03db9f57bb24bc655621562
razicommy <razi.com.my@gmail.com>
2018-03-02 06:08:12 +0800

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,27 +3,44 @@ angular.module('app.controllers', [])
 .controller('pageCtrl', ['$scope', '$stateParams', '$cordovaCamera', function ($scope, $stateParams, $cordovaCamera) {
     $scope.data={image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/220px-SNice.svg.png"}
 
-
+function displayImage(imageURI){
+$scope.data.image= imageURI;	
+}
+function generateImageData(imageURI){
+var testImage = new Image;	    
+testImage.onload = function() {
+    var canvas = document.createElement("canvas");
+    canvas.width = testImage.width;
+    canvas.height = testImage.height;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(testImage, 0, 0);
+    var dataUri = canvas.toDataURL('image/png');
+	console.log(dataUri)
+    $scope.data.testimage= 'data:image/png;base64,' + dataUri;	
+	$scope.$apply();
+	console.log($scope.data.testimage)
+};
+testImage.src = imageURI;  	
+}
+	
 $scope.captureImage=function(){
     
       document.addEventListener("deviceready", function () {
 
     var options = {
       quality: 50,
-      destinationType: Camera.DestinationType.DATA_URL,
+      destinationType: Camera.DestinationType.FILE_URI,
       sourceType: Camera.PictureSourceType.CAMERA,
       allowEdit: true,
       encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 100,
-      targetHeight: 100,
       saveToPhotoAlbum: false,
-	  correctOrientation:true
+	correctOrientation:true
     };
 
-    $cordovaCamera.getPicture(options).then(function(imageData) {
+    $cordovaCamera.getPicture(options).then(function(imageURI) {
 
-      $scope.data.image= "data:image/jpeg;base64," + imageData;
-	     
+      displayImage(imageURI);
+	generateImageData(imageURI);     
 	    
     }, function(err) {
       // error
